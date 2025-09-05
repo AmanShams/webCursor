81a414092e01fe347870113a731ef79d401bcce1
AmanDotDev <amanullahshamsi22@gmail.com>
2025-09-05 16:36:13 +0500

    feat: Implement template management with CRUD operations and add playground functionality

---
+++ lib/template/template-path.ts
@@ -0,0 +1,8 @@
+export const templatePaths = {
+  REACTJS: "/template-starters/react",
+  NEXTJS: "/template-starters/nextjs-new",
+  EXPRESS: "/template-starters/express-simple",
+  VUE: "/template-starters/vue",
+  HONO: "/template-starters/hono-nodejs-starter",
+  ANGULAR: "/template-starters/angular",
+};
